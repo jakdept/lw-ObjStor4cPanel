e07c2593f95d0573bd67ffb5f508dc3e49f2af4e
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-04-01 20:22:07 -0400

    tried reworking the tests quite a few ways, and still cannot get them to work

--- main_test.go
+++ main_test.go
@@ -5,36 +5,48 @@ import (
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"log"
-	"os"
+	//"os"
 	"testing"
 )
 
 var testingConfig runningConfig
 
-func TestMain(m *testing.M) {
-
+func loadTestingConfig() *runningConfig {
 	configContents, err := ioutil.ReadFile("testConfig.json")
 	if err != nil {
 		log.Printf("Failed to load my test configuration file at testConfig.json - %s", err)
 	}
+	testConfig := new(runningConfig)
 	err = json.Unmarshal(configContents, &testingConfig)
 	if err != nil {
 		log.Printf("Failed to load my test configuration file at testConfig.json - %s", err)
 	}
-	os.Exit(m.Run()) // call all the tests in here and then exit
+	return testConfig
 }
 
+//func TestMain(m *testing.M) {
+//func init() {
+
+//os.Exit(m.Run()) // call all the tests in here and then exit
+//}
+
 func TestSetupConnection(t *testing.T) {
+	testingConfig := loadTestingConfig()
 
 	connection := SetupConnection(testingConfig)
-	assert.Equal(t, connection.Auth.AccessKey, testingConfig.accessKey, "the Access Key should be the same")
-	assert.Equal(t, connection.Auth.SecretKey, testingConfig.secretKey, "the Secret Key should be the same")
+	assert.Equal(t, testingConfig.accessKey, connection.Auth.AccessKey, "the Access Key should be the same")
+	assert.Equal(t, testingConfig.secretKey, connection.Auth.SecretKey, "the Secret Key should be the same")
 
-	assert.Equal(t, connection.Region.S3Endpoint, "https://objects.liquidweb.services", "the URL should be LW's")
+	assert.Equal(t, "https://objects.liquidweb.services", connection.Region.S3Endpoint, "the URL should be LW's")
 }
 
 func TestValidBucket(t *testing.T) {
+	testingConfig := loadTestingConfig()
 	connection := SetupConnection(testingConfig)
+
+	_, err := connection.ListBuckets()
+
+	assert.Nil(t, err, "there should be no error listing the buckets")
 
 	assert.True(t, ValidBucket(connection, testingConfig), "the bucket should exist within the given space")
 }
