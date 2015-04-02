278e466067e320241897d20b419d55861d8a77e9
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-04-02 15:44:56 -0400

    wrote some new tests to replace my other tests that didn't work

--- main_test.go
+++ main_test.go
@@ -6,22 +6,19 @@ import (
 	"io/ioutil"
 	"log"
 	//"os"
+	//"reflect"
 	"testing"
 )
 
-var testingConfig runningConfig
+//var testingConfig runningConfig
 
-func loadTestingConfig() *runningConfig {
-	configContents, err := ioutil.ReadFile("testConfig.json")
+func loadTestingConfig(data []byte) runningConfig {
+	testingConfig := new(runningConfig)
+	err := json.Unmarshal(data, &testingConfig)
 	if err != nil {
-		log.Printf("Failed to load my test configuration file at testConfig.json - %s", err)
+		log.Printf("Failed to load a test configuration - %s", err)
 	}
-	testConfig := new(runningConfig)
-	err = json.Unmarshal(configContents, &testingConfig)
-	if err != nil {
-		log.Printf("Failed to load my test configuration file at testConfig.json - %s", err)
-	}
-	return testConfig
+	return *testingConfig
 }
 
 //func TestMain(m *testing.M) {
@@ -29,9 +26,40 @@ func loadTestingConfig() *runningConfig {
 
 //os.Exit(m.Run()) // call all the tests in here and then exit
 //}
+func TestLoadTestingConfig(t *testing.T) {
+	//configData := new([]byte)
+	//configData = `{"pwd":"/", "accessKey": "AccEssKey", "secretKey": "SecRetKey", "bucket": "BuKKiT"}`
+	configData := []byte(`{"pwd":"/", "accessKey": "AccEssKey", "secretKey": "SecRetKey", "bucket": "BuKKiT"}`)
+	testingConfig := loadTestingConfig(configData)
+
+	//assert.Equal(t, "runningConfig", string(reflect.TypeOf(testingConfig)), "the config should be of type runningConfig")
+	assert.Equal(t, "AccEssKey", testingConfig.accessKey, "the Access Key should be the same in the config")
+	assert.Equal(t, "SecRetKey", testingConfig.secretKey, "the Secret Key should be the same in the config")
+	assert.Equal(t, "/", testingConfig.pwd, "the pwd should be the same in the conf")
+}
 
 func TestSetupConnection(t *testing.T) {
-	testingConfig := loadTestingConfig()
+	//configData := new([]byte)
+	//configData = `{"pwd":"/", "accessKey": "AccEssKey", "secretKey": "SecRetKey", "bucket": "BuKKiT"}`
+	configData := []byte(`{"pwd":"/", "accessKey": "AccEssKey", "secretKey": "SecRetKey", "bucket": "BuKKiT"}`)
+	testingConfig := loadTestingConfig(configData)
+
+	connection := SetupConnection(testingConfig)
+	assert.Equal(t, "AccEssKey", connection.Auth.AccessKey, "the Access Key should be the same")
+	assert.Equal(t, "SecRetKey", connection.Auth.SecretKey, "the Secret Key should be the same")
+
+	assert.Equal(t, "https://objects.liquidweb.services", connection.Region.S3Endpoint, "the URL should be LW's")
+}
+
+/*
+func TestSetupConnection(t *testing.T) {
+	testingConfig := loadTestingConfig(`{"pwd":"/", "accessKey": "AccEssKey", "secretKey": "SecRetKey", "bucket": "stuff"}`)
+
+	log.Println(reflect.TypeOf(testingConfig))
+	log.Println(reflect.TypeOf(testingConfig.accessKey))
+	log.Println(testingConfig.accessKey)
+	//assert.Equal(t, runningConfig, testingConfig.(type), "the config is of the wrong type")
+	//testingConfig.(type)
 
 	connection := SetupConnection(testingConfig)
 	assert.Equal(t, testingConfig.accessKey, connection.Auth.AccessKey, "the Access Key should be the same")
@@ -39,14 +67,19 @@ func TestSetupConnection(t *testing.T) {
 
 	assert.Equal(t, "https://objects.liquidweb.services", connection.Region.S3Endpoint, "the URL should be LW's")
 }
+*/
 
 func TestValidBucket(t *testing.T) {
-	testingConfig := loadTestingConfig()
+	data, err := ioutil.ReadFile("testConfig.json")
+	if err != nil {
+		t.Error("failed to load my config file testConfig.json")
+	}
+	testingConfig := loadTestingConfig(data)
 	connection := SetupConnection(testingConfig)
 
-	_, err := connection.ListBuckets()
+	_, err = connection.ListBuckets()
 
 	assert.Nil(t, err, "there should be no error listing the buckets")
 
-	assert.True(t, ValidBucket(connection, testingConfig), "the bucket should exist within the given space")
+	assert.True(t, ValidBucket(testingConfig, connection), "the bucket should exist within the given space")
 }
