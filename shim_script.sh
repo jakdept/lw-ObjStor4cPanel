4c6d4c747c6e42204a4ddfa08b7b2b65bdce3af8
Jack Hayhurst <jakdept@gmail.com>
2017-07-20 23:06:59 -0400

    added a shim script (used for testing)

---
+++ shim_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]; then
+  echo 'you need to provide some parameters'
+  exit 1
+fi
+
+echo $@ >> /var/log/cpbackuptrans.log
+
+shift
+
+/scripts/cPanelToObjectStore $@
